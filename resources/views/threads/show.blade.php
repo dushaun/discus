f44a2a44c923026cd2bd630e33408781e2e00423
dushaun <dushaun.ac@gmail.com>
2017-07-07 15:17:46 +0100

    add replies to threads

--- resources/views/threads/show.blade.php
+++ resources/views/threads/show.blade.php
@@ -24,5 +24,23 @@
                 @endforeach
             </div>
         </div>
+
+        @if(auth()->check())
+            <div class="row">
+                <div class="col-md-8 col-md-offset-2">
+                    <form method="post" action="{{ $thread->path() . '/replies' }}">
+                        {{ csrf_field() }}
+                        <div class="form-group">
+                            <textarea name="body" id="body" placeholder="Reply here..." class="form-control" rows="4"></textarea>
+                        </div>
+                        <div class="form-group">
+                            <button type="submit" class="btn btn-default form-control">Submit</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        @else
+            <p class="text-center">Please <a href="{{ route('login') }}">login</a> or <a href="{{ route('register') }}">register</a> to get involved with this conversation.</p>
+        @endif
     </div>
 @endsection
