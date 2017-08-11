715914c1e8a313ee39f7dddbe096076f1d9f224e
dushaun <dushaun.ac@gmail.com>
2017-08-11 07:25:26 +0100

    adding view composer for channels

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use App\Channel;
+use Illuminate\Support\Facades\View;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -13,7 +15,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        View::composer('*', function ($view) {
+            $view->with('channels', Channel::all());
+        });
     }
 
     /**
